2b86700d4ae5a5842162aa56e8c7c9731482e258
neffer12 <andyneff12@gmail.com>
2025-05-02 16:31:48 -0500

    Add move to player

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 from constants import PLAYER_RADIUS
 from circleshape import CircleShape
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -30,3 +31,17 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt, -1)
+
+    def move(self, dt, direction=1):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt * direction
+
+
+
+
+        
